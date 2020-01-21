fba6637bab358a8b1c28b9fd5a3648108834259a
MrSprint <864047435@qq.com>
2020-01-21 22:26:14 +0800

    Q112 done

---
+++ 112.路径总和.py
@@ -0,0 +1,38 @@
+#
+# @lc app=leetcode.cn id=112 lang=python3
+#
+# [112] 路径总和
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+# Accepted
+# 114/114 cases passed (40 ms)
+# Your runtime beats 96.73 % of python3 submissions
+# Your memory usage beats 59.06 % of python3 submissions (15 MB)
+
+class Solution:
+    def hasPathSum(self, root: TreeNode, sum: int) -> bool:
+        if (not root):
+            return False
+        if not root.left and not root.right:
+            return (sum - root.val) == 0
+        left_bool = False
+        right_bool = False
+        if root.left:
+            left_bool = self.hasPathSum(root.left, sum - root.val)
+        if root.right:
+            right_bool = self.hasPathSum(root.right, sum - root.val)
+        # if not left_bool and right_bool:
+        #     return right_bool
+        # if not right_bool and left_bool:
+        #     return left_bool
+        return left_bool or right_bool
+# @lc code=end
+

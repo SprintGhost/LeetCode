d0f3a2c890519de6a915753ca4064a55825c9541
MrSprint <864047435@qq.com>
2020-01-24 21:11:00 +0800

    Q118 add others solution runtime

--- 118.杨辉三角.py
+++ 118.杨辉三角.py
@@ -10,26 +10,30 @@
 # Your memory usage beats 65.53 % of python3 submissions (13 MB)
 
 # @lc code=start
-# class Solution:
-#     def generate(self, numRows: int):
-#         result_list = list()
-#         for index in range(numRows):
-#             if index == 0:
-#                 result_list.append([1])
-#                 continue
-#             if index == 1:
-#                 result_list.append([1,1])
-#                 continue
-#             temp_list = list()
-#             temp_list.append(1)
-#             for index_temp in range(len(result_list[index - 1]) - 1):
-#                 temp_list.append(result_list[index - 1][index_temp] + result_list[index - 1][index_temp + 1])
-#             temp_list.append(1)
-#             result_list.append(temp_list)
-#         return result_list
+class Solution:
+    def generate(self, numRows: int):
+        result_list = list()
+        for index in range(numRows):
+            if index == 0:
+                result_list.append([1])
+                continue
+            if index == 1:
+                result_list.append([1,1])
+                continue
+            temp_list = list()
+            temp_list.append(1)
+            for index_temp in range(len(result_list[index - 1]) - 1):
+                temp_list.append(result_list[index - 1][index_temp] + result_list[index - 1][index_temp + 1])
+            temp_list.append(1)
+            result_list.append(temp_list)
+        return result_list
 
 # The following solution from leetcode
 # The solution is so awesome!
+# Accepted
+# 15/15 cases passed (32 ms)
+# Your runtime beats 82.82 % of python3 submissions
+# Your memory usage beats 65.53 % of python3 submissions (13.2 MB)
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
         if numRows == 0: return []

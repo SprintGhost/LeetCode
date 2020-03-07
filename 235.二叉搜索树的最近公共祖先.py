35b81e77b79b2e0f560540d710e4b4254a8d77f6
MrSprint <864047435@qq.com>
2020-03-07 22:29:16 +0800

    Q235 done

---
+++ 235.二叉搜索树的最近公共祖先.py
@@ -0,0 +1,49 @@
+#
+# @lc app=leetcode.cn id=235 lang=python3
+#
+# [235] 二叉搜索树的最近公共祖先
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+# Accepted
+# 27/27 cases passed (96 ms)
+# Your runtime beats 43.97 % of python3 submissions
+# Your memory usage beats 13.81 % of python3 submissions (17.5 MB)
+
+class Solution:
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        # Value of current node or parent node.
+        parent_val = root.val
+
+        # Value of p
+        p_val = p.val
+
+        # Value of q
+        q_val = q.val
+
+        # If both p and q are greater than parent
+        if p_val > parent_val and q_val > parent_val:    
+            return self.lowestCommonAncestor(root.right, p, q)
+        # If both p and q are lesser than parent
+        elif p_val < parent_val and q_val < parent_val:    
+            return self.lowestCommonAncestor(root.left, p, q)
+        # We have found the split point, i.e. the LCA node.
+        else:
+            return root
+
+
+# @lc code=end
+

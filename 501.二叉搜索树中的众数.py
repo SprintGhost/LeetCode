5f9472f5e8fb6324760fa186a59697b1399699b6
MrSprint <864047435@qq.com>
2020-05-16 21:34:04 +0800

    Q501 done

--- 501.二叉搜索树中的众数.py
+++ 501.二叉搜索树中的众数.py
@@ -4,6 +4,11 @@
 # [501] 二叉搜索树中的众数
 #
 
+# Accepted
+# 25/25 cases passed (72 ms)
+# Your runtime beats 59.49 % of python3 submissions
+# Your memory usage beats 20 % of python3 submissions (17.6 MB)
+
 # @lc code=start
 # Definition for a binary tree node.
 class TreeNode:
@@ -13,40 +18,37 @@ class TreeNode:
         self.right = None
 
 class Solution:
-    def __init__(self):
-        self.curTimes = 1
-        self.maxTimes = 0
-        self.res = list()
-    def inOrder(self, root, pre):
-        if (not root):
-            return
-        self.inOrder(root.left, pre)
-        if (pre):
-            if root.val == pre.val:
-                self.curTimes += 1
+    def findMode(self, root: TreeNode) -> List[int]:
+        if root is None:
+            return []
+        queue = [root]  
+        d = {} 
+        while queue:
+            node = queue.pop(0)
+
+            if node.val not in d:
+                d[node.val] = 1
             else:
-                self.curTimes = 1  
-        if (self.curTimes == self.maxTimes):
-            self.res.append(root.val)
-        elif (self.curTimes > self.maxTimes):
-            self.res = list()
-            self.res.append(root.val)
-            self.maxTimes = self.curTimes
-        self.inOrder(root.right, root)
+                d[node.val] +=1
 
-    def findMode(self, root: TreeNode):
-        if (not root):
-            return None
-        pre = None
-        self.inOrder(root, pre)
-        return self.res
+            if node.left is not None:
+                queue.append(node.left)
+            if node.right is not None:
+                queue.append(node.right)
+        
+        m = max(d.values())
+        l = []
+        for k,v in d.items():
+            if v == m:
+                l.append(k)
+        return l
 
-T = TreeNode(1)
-T_left = TreeNode(1)
-T.left = T_left
+# 作者：guo-guo-69
+# 链接：https://leetcode-cn.com/problems/find-mode-in-binary-search-tree/solution/cai-niao-ti-jie-by-guo-guo-69/
+# 来源：力扣（LeetCode）
+# 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+ 
 
-A = Solution()
-A.findMode(T)
 
       
 # @lc code=end

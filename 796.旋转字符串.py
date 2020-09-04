2060ea01a7dee5b7b34ecee30ff710bda530f869
MrSprint <864047435@qq.com>
2020-09-04 21:58:52 +0800

    Q796 done

--- 796.旋转字符串.py
+++ 796.旋转字符串.py
@@ -26,7 +26,19 @@ class Solution:
             if A[each:]+A[:each] == B:
                 return True
         return False
-        
+# Accepted
+# 45/45 cases passed (36 ms)
+# Your runtime beats 85.57 % of python3 submissions
+# Your memory usage beats 42.11 % of python3 submissions (13.7 MB)
+class Solution(object):
+    def rotateString(self, A, B):
+        return len(A) == len(B) and B in A+A
+
+# 作者：LeetCode
+# 链接：https://leetcode-cn.com/problems/rotate-string/solution/xuan-zhuan-zi-fu-chuan-by-leetcode/
+# 来源：力扣（LeetCode）
+# 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
 # A = Solution()
 # A.rotateString("bbbacddceeb","ceebbbbacdd")
 # @lc code=end

a1d39157a1ce49cbf2f94f7d6ec0c6f4c54296dd
MrSprint <864047435@qq.com>
2020-09-17 22:05:56 +0800

    Q832 add faster solution

--- 832.翻转图像.py
+++ 832.翻转图像.py
@@ -18,6 +18,26 @@ class Solution:
                     A[index_i][index_j] = 0
             A[index_i] = A[index_i][::-1]
         return A
+# Following solution is fanster than my solution
+# Accepted
+# 82/82 cases passed (60 ms)
+# Your runtime beats 88.5 % of python3 submissions
+# Your memory usage beats 46.82 % of python3 submissions (13.4 MB)
+class Solution(object):
+    def flipAndInvertImage(self, A):
+        for row in A:
+            for i in range((len(row) + 1) // 2):
+                """
+                In Python, the shortcut row[~i] = row[-i-1] = row[len(row) - 1 - i]
+                helps us find the i-th value of the row, counting from the right.
+                """
+                row[i], row[~i] = row[~i] ^ 1, row[i] ^ 1
+        return A
+
+# 作者：LeetCode
+# 链接：https://leetcode-cn.com/problems/flipping-an-image/solution/fan-zhuan-tu-xiang-by-leetcode/
+# 来源：力扣（LeetCode）
+# 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
 
 # A = Solution()
 # A.flipAndInvertImage([[1,1,0],[1,0,1],[0,0,0]])

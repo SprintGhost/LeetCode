56bcd0acd277bfec34d9c92e35761acbfcb89eb1
MrSprint <864047435@qq.com>
2020-12-14 22:20:53 +0800

    Q993 done

---
+++ 993.二叉树的堂兄弟节点.py
@@ -0,0 +1,42 @@
+#
+# @lc app=leetcode.cn id=993 lang=python3
+#
+# [993] 二叉树的堂兄弟节点
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+# Accepted
+# 104/104 cases passed (40 ms)
+# Your runtime beats 74.95 % of python3 submissions
+# Your memory usage beats 5.19 % of python3 submissions (15 MB)
+class Solution(object):
+    def isCousins(self, root, x, y):
+        parent = {}
+        depth = {}
+        def dfs(node, par = None):
+            if node:
+                depth[node.val] = 1 + depth[par.val] if par else 0
+                parent[node.val] = par
+                dfs(node.left, node)
+                dfs(node.right, node)
+
+        dfs(root)
+        return depth[x] == depth[y] and parent[x] != parent[y]
+
+# 作者：LeetCode
+# 链接：https://leetcode-cn.com/problems/cousins-in-binary-tree/solution/er-cha-shu-de-tang-xiong-di-jie-dian-by-leetcode/
+# 来源：力扣（LeetCode）
+# 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
+
+                
+
+        
+# @lc code=end
+
